843a81bf1914c1adfa40c6b596d9a8c1f0627982
Analia <analialauraenrique@gmail.com>
2025-05-11 10:15:43 -0300

    Especificado explícitamente index.html como punto de entrada en vite.config.js

--- 69600-react-aenrique/vite.config.js
+++ 69600-react-aenrique/vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, '.'), 
+  root: path.resolve(__dirname, '.'),
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
 })
