1809776c705994f450d2ea9428dfb373de3f4233
Analia <analialauraenrique@gmail.com>
2025-05-11 10:24:43 -0300

    Revertido a configuración básica de Vite

--- 69600-react-aenrique/vite.config.js
+++ 69600-react-aenrique/vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, '.'),
-  build: {
-    outDir: 'dist', 
-    emptyOutDir: true, 
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-      },
-    },
-  },
 })
