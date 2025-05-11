409329a15552bff4db1322d5332777362151c136
Analia <analialauraenrique@gmail.com>
2025-05-11 10:19:26 -0300

    Configuración de build más detallada en vite.config.js

--- 69600-react-aenrique/vite.config.js
+++ 69600-react-aenrique/vite.config.js
@@ -7,8 +7,17 @@ export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, '.'),
   build: {
+    outDir: 'dist', 
+    emptyOutDir: true, 
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
     },
   },
 })
